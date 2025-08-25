0a63543d49310c790655b6b24031173d979e80f9
Fritz Hesse <thereefulfreble@gmail.com>
2025-08-25 10:44:35 -0500

    added SP bar

--- UI/DemonData.gd
+++ UI/DemonData.gd
@@ -38,7 +38,7 @@ func _init(unit:battle_demon):
 	health_bar.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	health_bar.size_flags_vertical = Control.SIZE_EXPAND_FILL
 	health_bar.add_theme_stylebox_override("fill", StyleBoxFlat.new()) #stylebox needed for color
-	health_bar.get_theme_stylebox("fill").set("bg_color", Color("#7eab1e"))
+	health_bar.get_theme_stylebox("fill").set("bg_color", Color("#4CBB17"))
 	health_bar.max_value = unit.MHP
 	health_bar.step = 1
 	health_bar.value = unit.HP
@@ -50,9 +50,42 @@ func _init(unit:battle_demon):
 	health_hbox.add_child(health_data)
 	vbox.add_child(health_hbox)
 	#endregion
+	
+	#region SP BAR
+	#create container and label
+	var sp_hbox = HBoxContainer.new()
+	sp_hbox.set_anchors_preset(PRESET_FULL_RECT)
+	var sp_label = Label.new()
+	sp_label.text = "SP"
+	sp_hbox.add_child(sp_label)
+	
+	#create bar
+	sp_bar.fill_mode = sp_bar.FILL_BEGIN_TO_END
+	sp_bar.show_percentage = false
+	sp_bar.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	sp_bar.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	sp_bar.add_theme_stylebox_override("fill", StyleBoxFlat.new()) #stylebox needed for color
+	sp_bar.get_theme_stylebox("fill").set("bg_color", Color("#C229C2"))
+	sp_bar.max_value = unit.MSP
+	sp_bar.step = 1
+	sp_bar.value = unit.SP
+	sp_hbox.add_child(sp_bar)
+	
+	#create text readout for data
+	sp_data.text = "%d/%d" % [unit.SP, unit.MSP]
+	sp_data.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+	sp_hbox.add_child(sp_data)
+	vbox.add_child(sp_hbox)
+	#endregion
 
 func update_hp(new_hp:int):
 	#sets health bar fill
 	health_bar.value = new_hp
 	#sets data readout
 	health_data.text = "%d/%d" % [new_hp, tied_unit.MHP]
+
+func update_sp(new_sp:int):
+	#sets sp bar fill
+	sp_bar.value = new_sp
+	#sets data readout
+	sp_data.text = "%d/%d" % [new_sp, tied_unit.MSP]
